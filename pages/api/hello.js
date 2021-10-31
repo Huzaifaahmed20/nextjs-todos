42500717b9aa17531bf1deb27cce7c4fcaf8f505
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2021-11-01 00:51:14 +0500

    api data change

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,40 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    name: "John Doe",
+    todos: [
+      {
+        userId: 1,
+        id: 1,
+        title: "delectus aut autem",
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: "quis ut nam facilis et officia qui",
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 3,
+        title: "fugiat veniam minus",
+        completed: false,
+      },
+      {
+        userId: 1,
+        id: 4,
+        title: "et porro tempora",
+        completed: true,
+      },
+      {
+        userId: 1,
+        id: 5,
+        title:
+          "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        completed: false,
+      },
+    ],
+  });
 }
