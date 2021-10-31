02016b035aea86f15bb10865e1272626e0a2e4a4
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2021-11-01 01:01:22 +0500

    api data change createdAt cache

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.set("Cache-Control", "public, s-maxage=300"); // 5 min
+  res.setHeader("Cache-Control", "public, s-maxage=300"); // 5 min
 
   res.status(200).json({
     name: "John Doe",
