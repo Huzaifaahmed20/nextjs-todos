eb66ac9fdec746dfb7770eb0a88ed5d229cf700c
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2021-10-31 22:55:49 +0500

    random api integrate

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,32 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home({ todos }) {
+export default function Home({ todo }) {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>Todos</h1>
-        <ul>
-          {todos.map((todo) => {
-            return <li key={todo?.id}>{todo?.title}</li>;
-          })}
-        </ul>
+        <h3>{todo?.id}</h3>
+        <h3>{todo?.title}</h3>
       </main>
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-  const todos = await (
-    await fetch("https://jsonplaceholder.typicode.com/todos")
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const rndInt = randomIntFromInterval(1, 200);
+
+  const todo = await (
+    await fetch(`https://jsonplaceholder.typicode.com/todos/${rndInt}`)
   ).json();
 
   return {
     props: {
-      todos,
+      todo,
     }, // will be passed to the page component as props
   };
 }
